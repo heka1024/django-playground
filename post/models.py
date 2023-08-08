f7b82638051e946368e8106ad164c40cf64665d9
heka1024 <heka1024@gmail.com>
2023-08-08 15:46:31 +0900

    Add `Like`

--- post/models.py
+++ post/models.py
@@ -7,3 +7,11 @@ class Post(BaseModel):
     title = models.CharField(max_length=100, blank=True, null=False)
     content = models.TextField(blank=True, null=False)
     user = models.ForeignKey('user.User', on_delete=models.DO_NOTHING, related_name='posts', null=True)
+
+
+class Like(BaseModel):
+    post = models.ForeignKey('post.Post', on_delete=models.CASCADE, related_name='likes', null=False)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='likes', null=False)
+
+    class Meta:
+        unique_together = ('post', 'user')
