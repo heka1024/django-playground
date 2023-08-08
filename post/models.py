da7ea9d745d3b9873c9a7bc715c767084f6fba14
heka1024 <heka1024@gmail.com>
2023-08-08 15:01:32 +0900

    Add `Post` model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from playground.models import BaseModel
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=100, blank=True, null=False)
+    content = models.TextField(blank=True, null=False)
+    user = models.ForeignKey('user.User', on_delete=models.DO_NOTHING, related_name='posts', null=True)
