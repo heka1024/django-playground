e08e0d9bbafcb0b253bbe57ff9e75c539581d8a4
heka1024 <heka1024@gmail.com>
2023-08-23 01:41:00 +0900

    Test composite primary key

--- post/models.py
+++ post/models.py
@@ -15,3 +15,17 @@ class Like(BaseModel):
 
     class Meta:
         unique_together = ('post', 'user')
+
+
+class Scrap(BaseModel):
+    """
+    Composite Primary Key 테스트
+
+    post에 primary key = True가 걸려있지만 사실 primary key는 post와 user의 조합이다.
+    장고가 Composite Primary Key를 지원하지 않기 때문
+    """
+    post = models.ForeignKey('post.Post', on_delete=models.CASCADE, related_name='scraps', null=False, primary_key=True)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='scraps', null=False)
+
+    class Meta:
+        models.UniqueConstraint(fields=['post', 'user'], name='unique_scrap')
