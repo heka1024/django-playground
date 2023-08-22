f23752615eb78351f33a628228f067031b1032e9
heka1024 <heka1024@gmail.com>
2023-08-23 01:22:31 +0900

    LIike

--- user/models.py
+++ user/models.py
@@ -2,9 +2,13 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 from playground.models import BaseModel
+from post.models import Like
 
 
 class User(BaseModel, AbstractBaseUser):
     username = models.CharField(max_length=100, blank=True, null=False)
 
     USERNAME_FIELD = 'username'
+
+    def like(self, post):
+        Like.objects.create(post=post, user=self)
